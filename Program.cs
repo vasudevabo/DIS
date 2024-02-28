297c6a5c4c8ad8afbb42e24821d2c4e9f2739433
Vasudeva Reddy Bolleddula <142105629+vasudevabo@users.noreply.github.com>
2024-02-28 11:29:17 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿/* Here is the Bubble sort code 
+ * Auther:Vasudeva reddy Bolleddula
+ * Publish: 28th Feb 2024*/
+
+
+
+
+using System;
+
+class BubbleSort
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 5,72,3,14,7,1 };
+
+        Console.WriteLine("Here is the original array that will be used to execute the Bubble Sort Algorithm:");
+        PrintArray(array);
+
+        BubbleSortArray(array);
+
+        Console.WriteLine("\nHere is the array after the execution of the Bubble Sort Algorithm:");
+        PrintArray(array);
+    }
+
+    static void BubbleSortArray(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    (arr[j + 1], arr[j]) = (arr[j], arr[j + 1]);
+                }
+            }
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
